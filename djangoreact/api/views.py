7b258aa0afe8a94e4e54ab55eb62f633ebf7f6cf
HYOGONKIM <gonaldo13@gmail.com>
2021-11-17 22:29:45 +0900

    feat: 회원가입 기능 추가

--- djangoreact/api/views.py
+++ djangoreact/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from .serializers import UserCreateSerializer
+from .models import User
 
-# Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
+@method_decorator(csrf_exempt, name='dispatch')
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def createUser(request):
+    if request.method == 'POST':
+        serializer = UserCreateSerializer(data=request.data)
+        if not serializer.is_valid(raise_exception=True):
+            return Response({"message": "Request Body Error."}, status=status.HTTP_409_CONFLICT)
+
+        if User.objects.filter(email=serializer.validated_data['email']).first() is None:
+            serializer.save()
+            return Response({"message": "ok"}, status=status.HTTP_201_CREATED)
+        return Response({"message": "duplicate email"}, status=status.HTTP_409_CONFLICT)
