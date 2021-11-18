9150db470486c2a8002228ea6b7cda9d8583829b
minji <my970524@naver.com>
2021-11-18 15:08:58 +0900

    feat : 로그인 기능 추가
    Close  #17

--- djangoreact/api/urls.py
+++ djangoreact/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('create/', views.createUser),
+    path('login/', views.login)
 ]
--- djangoreact/api/views.py
+++ djangoreact/api/views.py
@@ -2,12 +2,13 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
-from .serializers import UserCreateSerializer
+from .serializers import UserCreateSerializer, UserLoginSerializer
 from .models import User
 
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
+# 회원가입 
 @method_decorator(csrf_exempt, name='dispatch')
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -21,3 +22,21 @@ def createUser(request):
             serializer.save()
             return Response({"message": "ok"}, status=status.HTTP_201_CREATED)
         return Response({"message": "duplicate email"}, status=status.HTTP_409_CONFLICT)
+
+# 로그인
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login(request):
+    if request.method == 'POST':
+        serializer = UserLoginSerializer(data=request.data)
+
+        if not serializer.is_valid(raise_exception=True):
+            return Response({'message': 'Request Body Error.'}, status=status.HTTP_409_CONFLICT)
+        if serializer.validated_data['email'] == 'None':
+            return Response({'message': 'fail'}, status=status.HTTP_200_OK)
+        
+        response = {
+            'success': 'True',
+            'token': serializer.data['token']
+        }
+        return Response(response, status=status.HTTP_200_OK)
