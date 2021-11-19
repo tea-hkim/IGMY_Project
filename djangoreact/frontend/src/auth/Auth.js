3108b5165745f358419dff08fee9f67e8530b750
im_TaeHo_K <81206228+tea-hkim@users.noreply.github.com>
2021-11-19 15:12:53 +0900

    feat front #34 : 로그인 뼈대 구축

--- djangoreact/frontend/src/auth/Auth.js
+++ djangoreact/frontend/src/auth/Auth.js
@@ -22,16 +22,19 @@ const Auth = () => {
       <button type="button">카카오 계정으로 로그인</button>
       <div>또는</div>
       <form onSubmit={onSubmit}>
-        <input name="email" type="text" placeholder="이메일 주소" required value={email} onChange={onChange} />
-        <input name="password" type="password" placeholder="페스워드" required={password} onChange={onChange} />
+        <input name="email" type="text" placeholder="이메일" required value={email} onChange={onChange} />
+        <input name="password" type="password" placeholder="비밀번호" required={password} onChange={onChange} />
         <div>자동로그인</div>
         <input type="submit" value="Log In" />
       </form>
       <div>
-        <span>아직 이게모약 계정이 없으신가요?</span>
-        <span>가입하기</span>
+        <div>아직 이게모약 계정이 없으신가요?</div>
+        <div>가입하기</div>
       </div>
-      <div>혹시 비밀번호를 잊으셨나요?</div>
+      <div>
+        <div>혹시 비밀번호를 잊으셨나요?</div>
+        <div>비밀번호 재설정</div>
+      </div>
     </>
   );
 };
