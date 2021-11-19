175bb895d4d5ed7d55b487539500c6baf28fbf91
kaky <kakymo22@gmail.com>
2021-11-19 10:19:31 +0900

    feat : 리덕스 테스트 버전 세팅
    리덕스 툴킷을 이용한 기본 테스트 세팅

--- djangoreact/frontend/src/App.js
+++ djangoreact/frontend/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import TestRedux from './components/TestRedux';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <TestRedux />
       </header>
     </div>
   );
