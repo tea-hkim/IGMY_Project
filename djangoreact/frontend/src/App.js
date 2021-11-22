ef49e6f59736e7ab0872980dc43627365b0797ae
KIMHYOGON <gonaldo13@gmail.com>
2021-11-22 15:36:03 +0900

    Feat: 메인페이지 틀 잡기

--- djangoreact/frontend/src/App.js
+++ djangoreact/frontend/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import MainPage from './components/MainPage';
 import Auth from './auth/Auth';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          <Auth />
-        </p>
-      </header>
+      <Navigation />
+      <h1>App</h1>
+      <ul>
+        <li>
+          <Link to="/">메인 페이지</Link>
+        </li>
+        <li>
+          <Link to="/login">로그인 페이지</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route exact path="/" element={<MainPage />} />
+        <Route path="/login" element={<Auth />} />
+      </Routes>
     </div>
   );
 }
--- djangoreact/frontend/src/index.js
+++ djangoreact/frontend/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
 import App from './App';
-import store from './redux/store';
+// import store from './redux/store';
 
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <BrowserRouter>
+    {/* <Provider store={store}> */}
+    <App />
+    {/* </Provider> */}
+  </BrowserRouter>,
   rootElement,
 );
