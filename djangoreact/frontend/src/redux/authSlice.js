857e5851428229a9c831e87339248a9345d15dcc
im_TaeHo_K <81206228+tea-hkim@users.noreply.github.com>
2021-11-24 16:48:03 +0900

    feat front #34   : 로그인 및 회원가입 axios 구현

--- djangoreact/frontend/src/redux/authSlice.js
+++ djangoreact/frontend/src/redux/authSlice.js
@@ -6,12 +6,14 @@ export const authSlice = createSlice({
     email: '',
     password: '',
     nickname: '',
+    token: '',
   },
   reducers: {
     login: (state, action) => {
       const newState = state;
       newState.email = action.payload.email;
       newState.password = action.payload.password;
+      newState.token = action.payload.token;
       state = newState;
     },
     register: (state, action) => {
