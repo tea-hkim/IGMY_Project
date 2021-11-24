b40f58401972325ef5e6c2f603b4092abe1cfd16
KIMHYOGON <gonaldo13@gmail.com>
2021-11-24 18:57:43 +0900

    db모델 및 pip 호환성 해결

--- djangoreact/api/urls.py
+++ djangoreact/api/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('search-all/', views.search_all),
     path('search-direct/', views.search_direct),
     # OAuth : kakao api
-    path("login/kakao", views.kakao_login, name="kakao_login"),
-    path("login/kakao/callback", views.kakao_callback, name="kakao_callback"),
+    #path("login/kakao", views.kakao_login, name="kakao_login"),
+    #path("login/kakao/callback", views.kakao_callback, name="kakao_callback"),
     # path('login/kakao/finish/', views.kakao_login_finish.as_view(),
     #      name='kakao_login_todjango'),
-    path("/pill-detail/<string:pill-id>", views.pill_detail),
+    #path("/pill-detail/<string:pill-id>", views.pill_detail),
 ]
