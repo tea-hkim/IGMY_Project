c8fbffbbb5300f523a7463ec30d944cfc67e9df2
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-12-04 03:51:06 +0900

    feat : pull and resolve conflict
    Former-commit-id: 8af02e7120d4897cc52df712e75611c99d5c23c2

--- djangoreact/api/models.py
+++ djangoreact/api/models.py
@@ -52,7 +52,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(
         _("staff status"),
         default=False,
-        help_text=_("Designates whether the user can log into this admin site."),
+        help_text=_(
+            "Designates whether the user can log into this admin site."),
     )
     is_active = models.BooleanField(
         _("active"),
@@ -147,5 +148,3 @@ class SearchHistory(models.Model):
         InfoPill, to_field="item_num", db_column="pill_num", on_delete=models.CASCADE
     )
     create_at = models.DateField(auto_now_add=True)
-
-
