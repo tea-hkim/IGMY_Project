490c2c4f9a8e67e9d50f319239884f8c82520308
HYOGONKIM <gonaldo13@gmail.com>
2021-12-08 01:53:34 +0900

    fix 회원가입 오류문 리팩토링
    Former-commit-id: cfae3dc2e4f75ced9e525ef2045d75f5ea2cf971

--- djangoreact/api/views.py
+++ djangoreact/api/views.py
@@ -52,15 +52,9 @@ def createUser(request):
     if request.method == "POST":
         serializer = UserCreateSerializer(data=request.data)
         if not serializer.is_valid(raise_exception=True):
-            return Response(
-                {"message": "Request Body Error."}, status=status.HTTP_409_CONFLICT
-            )
+            return Response({"message": "Request Body Error."}, status=status.HTTP_400_CONFLICT)
 
-        if (
-            User.objects.filter(
-                email=serializer.validated_data["email"]).first()
-            is None
-        ):
+        if (User.objects.filter(email=serializer.validated_data["email"]).first() is None):
             serializer.save()
             return Response({"message": "ok"}, status=status.HTTP_201_CREATED)
         return Response({"message": "duplicate email"}, status=status.HTTP_200_OK)
