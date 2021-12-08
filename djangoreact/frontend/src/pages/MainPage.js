3ba15ac16ef05a889149159958b66ae62fdf4b93
im_TaeHo_K <xogh8025@naver.com>
2021-12-08 18:51:09 +0900

    style front #38 : 메인페이지 스타일 변경
    Former-commit-id: 968a0a78a4c72229383feb06d52b462d73b4f7dc

--- djangoreact/frontend/src/pages/MainPage.js
+++ djangoreact/frontend/src/pages/MainPage.js
@@ -1,27 +1,42 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import * as mdIcons from 'react-icons/md';
+import * as imIcons from 'react-icons/im';
 import { MainContainer, Box, Circle, Giyeog, Square } from '../styles/MainPageStyle';
 
 const MainPage = () => {
   const navigate = useNavigate();
   return (
     <MainContainer className="main_container">
-      <h2 className="main_container_title">
-        간편하고 손 쉬운
-        <br />내 손안의 작은 알약 사전
-      </h2>
+      <h1 className="main_container_title">이게모약</h1>
       <Box className="main_container_box">
         <Circle className="main_container_box_picture" onClick={() => navigate('/camera')}>
-          사진 검색
+          <img src="images/pill.png" alt="알약" />
+          <div>
+            <mdIcons.MdOutlineImageSearch size="4rem" />
+            사진 검색
+          </div>
         </Circle>
-        <Giyeog className="main_container_box_introduce" onClick={() => navigate('/')}>
-          {' '}
+        <Giyeog className="main_container_box_introduce" onClick={() => navigate('/about-us')}>
+          <div>
+            팀원
+            <br />
+            소개
+          </div>
         </Giyeog>
-        <Square className="main_container_box_rectangle" onClick={() => navigate('/')}>
-          {' '}
+        <Square className="main_container_box_rectangle" onClick={() => navigate('/about-service')}>
+          <div>
+            서비스
+            <br />
+            소개
+          </div>
         </Square>
         <Circle className="main_container_box_search" onClick={() => navigate('/direct')}>
-          직접 검색
+          <img src="images/pill.png" alt="알약" />
+          <div>
+            <imIcons.ImSearch size="4rem" />
+            직접 검색
+          </div>
         </Circle>
       </Box>
     </MainContainer>
