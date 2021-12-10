b5a75bf631999bb010ecd7baa9aab212bac3f551
kaky <kakymo22@gmail.com>
2021-12-10 18:39:05 +0900

    feat : 알약 스캔 페이지 기능 추가
    인공지능 모델 후보 5개까지 받아와서 보기로 나타내기
    
    
    Former-commit-id: 43132becbb645b5aed60bbf4e2b5c511a346cd68

--- djangoreact/frontend/src/pages/PillDetailPage.js
+++ djangoreact/frontend/src/pages/PillDetailPage.js
@@ -77,7 +77,7 @@ const PillDetailPage = () => {
   const handleUserPill = async () => {
     if (!isUserPill) {
       try {
-        const response = await axios.post(`${REACT_APP_HOST_IP_ADDRESS}user-pill/?pn=${pillNum}`, pillNum, {
+        const response = await axios.post(`${REACT_APP_HOST_IP_ADDRESS}api/user-pill/?pn=${pillNum}`, pillNum, {
           headers: {
             Authorization: `Bearer ${access}`,
           },
@@ -89,7 +89,7 @@ const PillDetailPage = () => {
       }
     } else {
       try {
-        const response = await axios.delete(`${REACT_APP_HOST_IP_ADDRESS}user-pill/?pn=${pillNum}`, {
+        const response = await axios.delete(`${REACT_APP_HOST_IP_ADDRESS}api/user-pill/?pn=${pillNum}`, {
           headers: {
             Authorization: `Bearer ${access}`,
           },
