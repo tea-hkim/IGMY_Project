7c401ceecb41f857451af261f089d976a0656ef6
im_TaeHo_K <xogh8025@naver.com>
2021-12-11 12:56:39 +0900

    refactor front : 패스워드 리셋 페이지 유효성 검사 기능 수정
    Former-commit-id: 52e1ac0daddccbbb837bc3bf0f31b8b14a512fb7

--- djangoreact/frontend/src/pages/ResetPasswordConfimPage.js
+++ djangoreact/frontend/src/pages/ResetPasswordConfimPage.js
@@ -2,21 +2,20 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { REACT_APP_HOST_IP_ADDRESS } from '../env';
 import { resetpasswordconfirm } from '../redux/authSlice';
 
 const ResetPasswordConfirmPage = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const { uid, token } = useParams();
-  const [requestSent, setRequestSent] = useState(false);
+  const [isSame, setIsSame] = useState('');
   const [formData, setFormData] = useState({
-    new_password: '',
-    re_new_password: '',
+    newPassword: '',
+    reNewPassword: '',
   });
-  // const [isActive, setIsActive] = useState(false);
-  // eslint-disable-next-line camelcase
-  const { new_password, re_new_password } = formData;
+  const { newPassword, reNewPassword } = formData;
 
   const resetPasswordConfirm = async () => {
     const config = {
@@ -24,13 +23,10 @@ const ResetPasswordConfirmPage = () => {
         'Content-Type': 'application/json',
       },
     };
-
-    const body = JSON.stringify({ uid, token, new_password });
-
+    const body = JSON.stringify({ uid, token, newPassword });
     try {
       await axios.post(`${REACT_APP_HOST_IP_ADDRESS}auth/users/reset_password_confirm/`, body, config);
-
-      dispatch(resetpasswordconfirm({ uid, token, new_password }));
+      dispatch(resetpasswordconfirm({ uid, token, newPassword }));
     } catch (error) {
       alert('요청이 잘못 되었습니다.');
     }
@@ -38,56 +34,45 @@ const ResetPasswordConfirmPage = () => {
 
   const onSubmit = (event) => {
     event.preventDefault();
-
-    resetPasswordConfirm(uid, token, new_password);
-    setRequestSent(true);
+    if (newPassword !== reNewPassword) {
+      setIsSame('비밀번호가 일치하지 않습니다');
+      return;
+    }
+    resetPasswordConfirm(uid, token, newPassword);
+    navigate('/login');
   };
 
   const onChange = (event) => setFormData({ ...formData, [event.target.name]: event.target.value });
 
-  // if (formData.new_password === formData.re_new_password) {
-  //   setIsActive(true);
-  // }
-
-  if (requestSent) {
-    return <useNavigate to="/login" />;
-  }
-
   return (
     <PasswordConfirmDiv>
       <h1>새 비밀번호를 설정해주세요 🔐</h1>
-      <br />
       <form onSubmit={(event) => onSubmit(event)}>
         <FormField>
           <input
             className="form-control1"
             type="password"
             placeholder="새 비밀번호"
-            name="new_password"
-            // eslint-disable-next-line camelcase
-            value={new_password}
-            onChange={(event) => onChange(event)}
+            name="newPassword"
+            value={newPassword}
             pattern="^(?=.*[a-zA-Z])[0-9a-zA-Z]{8,50}$"
+            onChange={(event) => onChange(event)}
             required
           />
-          <span />
         </FormField>
-        <br />
         <FormField>
           <input
             className="form-control2"
             type="password"
             placeholder="새 비밀번호 확인"
-            name="re_new_password"
-            // eslint-disable-next-line camelcase
-            value={re_new_password}
+            name="reNewPassword"
+            value={reNewPassword}
             onChange={(event) => onChange(event)}
             pattern="^(?=.*[a-zA-Z])[0-9a-zA-Z]{8,50}$"
             required
           />
-          <span />
         </FormField>
-        <br />
+        <h2>{isSame}</h2>
         <Button className="btn btn-primary" type="submit">
           비밀번호 재설정
         </Button>
@@ -100,73 +85,48 @@ export default ResetPasswordConfirmPage;
 
 const PasswordConfirmDiv = styled.div`
   width: 100%;
-  height: 65vh;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  div {
+    margin-bottom: 1rem;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+  h2 {
+    color: red;
+    margin: 0 auto 1rem;
+  }
 `;
 
 const Button = styled.button`
-  box-shadow: inset 0px 1px 0px 0px #ffffff;
-  background: linear-gradient(to bottom, #ffffff 5%, #f6f6f6 100%);
-  background-color: #ffffff;
+  background-color: lightgray;
   border-radius: 6px;
   border: 1px solid #dcdcdc;
-  display: inline-block;
   cursor: pointer;
   color: #666666;
-  font-family: Arial;
-  font-size: 15px;
+  font-size: 1rem;
+  margin: 0 auto;
   font-weight: bold;
-  padding: 6px 24px;
-  text-decoration: none;
-  text-shadow: 0px 1px 0px #ffffff;
+  padding: 0.625rem 1.5rem;
 
-  &:hover {
-    background: linear-gradient(to bottom, #f6f6f6 5%, #ffffff 100%);
-    background-color: #f6f6f6;
-  }
   &:active {
-    position: relative;
+    background: linear-gradient(to bottom, #ffffff 5%, #f6f6f6 100%);
     top: 1px;
   }
 `;
 
 const FormField = styled.div`
   position: relative;
-  > input {
+  input {
     border-radius: 0.25em;
     border-style: solid;
     border-width: 2px;
     font-size: 1.5rem;
     padding: 0.5em 4em 0.5em 2em;
-
-    :valid {
-      border-color: forestgreen;
-
-      + span::after {
-        position: absolute;
-        right: 1em;
-        top: 50%;
-        transform: translateY(-50%);
-        content: '✔️';
-      }
-    }
-
-    :invalid {
-      border-color: firebrick;
-
-      + span::after {
-        position: absolute;
-        right: 1em;
-        top: 50%;
-        transform: translateY(-50%);
-        content: '❌';
-      }
-    }
-  }
-  > span {
-    margin-left: 10px;
   }
 `;
