9e8df4517171e142a3cad446c8d848cd5ca2044c
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-12-11 22:42:13 +0900

    fix kakao social login
    Former-commit-id: 4b7d7e46c36743c192c7552343b1303d61884247

--- djangoreact/backend/manage.py
+++ djangoreact/backend/manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import dotenv
 
 
 def main():
@@ -30,4 +31,5 @@ def main():
 
 
 if __name__ == '__main__':
+    dotenv.read_dotenv()
     main()
