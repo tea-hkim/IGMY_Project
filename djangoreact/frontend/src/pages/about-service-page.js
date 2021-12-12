6896a6de1d7da950b4996ca4ce63da3ddf3bcacb
im_TaeHo_K <xogh8025@naver.com>
2021-12-13 03:36:28 +0900

    style front : 서비스 기획 의도 페이지 반응형
    Former-commit-id: e89cc832f9b0e41331bcb45c7dd22f9ff40c6558

--- djangoreact/frontend/src/pages/about-service-page.js
+++ djangoreact/frontend/src/pages/about-service-page.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import WhiteNavbar from '../components/WhiteNavbar';
+import { mainPink } from '../styles/color';
 
 const Section = ({ num }) => {
   const imgURL = `service/service_${num}.png`;
@@ -22,6 +23,14 @@ const AboutServicePage = () => {
         {serviceList.map((num) => {
           return <Section num={num} />;
         })}
+        <button
+          type="button"
+          onClick={() => {
+            window.location.href = '/';
+          }}
+        >
+          이게모약 이용하기
+        </button>
       </AboutServiceContainer>
     </>
   );
@@ -30,20 +39,46 @@ const AboutServicePage = () => {
 export default AboutServicePage;
 
 const AboutServiceContainer = styled.div`
+  position: relative;
   background-color: white;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  button {
+    position: absolute;
+    bottom: 1%;
+    font-size: 1.25rem;
+    border: none;
+    border-radius: 5px;
+    outline: none;
+    padding: 0.313em 2em;
+    color: white;
+    background-color: ${mainPink};
+    box-shadow: 2px 2px 3px 2px rgba(0, 0, 0, 0.4);
+    cursor: pointer;
+  }
+  @media only screen and (max-width: 48rem) {
+    button {
+      font-size: 0.625em;
+      padding: 0.313em 1em;
+    }
+  }
 `;
 
 const ImgBox = styled.div`
-  height: 90vh;
-  margin: 0;
+  display: flex;
+  justify-content: center;
+  width: 70%;
   box-sizing: border-box;
-  padding: 0;
+  @media only screen and (max-width: 48rem) {
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
 `;
 
 const ContentImg = styled.img`
   height: 100%;
+  overflow: hidden;
 `;
