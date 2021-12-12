6acb05c366420d06a5796972f235ca1cc9d15dec
[2기-1반] 강석영 <ksge1124@gmail.com>
2021-12-12 19:23:05 +0900

    fix social login
    Former-commit-id: dd033940f8b50c5ee47a58fade49f5d4d414a478

--- djangoreact/backend/api/views.py
+++ djangoreact/backend/api/views.py
@@ -158,12 +158,9 @@ def user_pill(request):
     }
 
     user_email = request.user  # 유저 불러오기
-    print(user_email)
 
     if "#$%" in user_email.email:
         user_email.email = user_email.email.split("#$%")[1]
-
-    print(user_email.email)
 
     pill = InfoPill.objects.all()  # 약 정보 데이터 베이스 전부 가져오기
     pn = request.GET.get("pn", "")  # 약 넘버
